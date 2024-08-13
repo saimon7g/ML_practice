bb41c8d1e19bad29218da6e54a3cd32cf49a5bd6
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-08-14 01:16:48 +0600

    runscreipt created

---
+++ runscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+# source newenv/bin/activate
+
+# Run the Python script
+python test.py
+
+# Deactivate the virtual environment (optional)
+# deactivate
